93dbd3c61156e5b8efd3e544657d29cfbf5df762
Juli <820917549@qq.com>
2018-04-12 08:25:37 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '元尊': 'https://www.piaotian.com/html/8/8955/index.html',
-  '飞剑问道': 'https://www.piaotian.com/html/8/8952/index.html',
-  '剑来': 'https://www.piaotian.com/html/8/8717/index.html'
-};
